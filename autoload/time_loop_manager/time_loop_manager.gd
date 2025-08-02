26e34b7fd5375140fbcc01a48299fe8845640a67
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-08-02 11:16:57 +0300

    Time loopiness

--- autoload/time_loop_manager/time_loop_manager.gd
+++ autoload/time_loop_manager/time_loop_manager.gd
@@ -1,6 +1,7 @@
 ## Handles time, space, and flying blue police call boxes
 class_name TimeManagerType extends Node
 
+signal prereset()
 signal reset()
 signal time_advanced(new_hour: int)
 
@@ -11,16 +12,25 @@ const END_PM := 7
 var counter: float = 0.0
 var time: int = 0
 
+var _called_prereset := false
+
 func _process(delta: float) -> void:
 	counter += delta
 	
-	var new_time := int(remap(counter, 0, MAX_TIME, START_PM, END_PM))
+	var new_time_float := remap(counter, 0, MAX_TIME, START_PM, END_PM)
+	var new_time := int(new_time_float)
 	if new_time > time:
 		time_advanced.emit(new_time)
 	time = new_time
 	
+	if counter > (MAX_TIME - 0.05) and not _called_prereset:
+		prereset.emit()
+		_called_prereset = true
+	
+	# Time reset
 	if time > END_PM:
 		time = 0
 		counter = 0
 		(DialogManager as DialogManagerType).close()
 		reset.emit()
+		_called_prereset = false
--- autoload/time_loop_manager/tracker/time_loop_tracker.gd
+++ autoload/time_loop_manager/tracker/time_loop_tracker.gd
@@ -4,6 +4,8 @@ extends CanvasLayer
 
 @export_group("Local")
 @export var music: AudioStreamPlayer
+@export var reset_layer: CanvasLayer
+@export var reset_anim: AnimationPlayer
 @export var counter_label: Label
 
 var music_stream: AudioStreamInteractive
@@ -16,6 +18,13 @@ func _ready() -> void:
 	music_playback = music.get_stream_playback()
 	music_playback.switch_to_clip(0)
 	music_stages = music_stream.clip_count
+	reset_layer.visible = false
+	(TimeManager as TimeManagerType).prereset.connect(func():
+		reset_anim.play("time_loop_prereset")
+	)
+	(TimeManager as TimeManagerType).reset.connect(func():
+		reset_anim.play("time_loop_reset")
+	)
 
 func _process(delta: float) -> void:
 	var stage: int = remap(TimeManager.counter, 0, TimeManager.MAX_TIME, 0, music_stages)
