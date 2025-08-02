3f1f8c8d2cb9d9543d30ed0c1ff01712c3d63594
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-08-02 21:07:20 +0300

    Add name to item slot

--- entities/player/inventory_ui/inventory_item_slot.gd
+++ entities/player/inventory_ui/inventory_item_slot.gd
@@ -1,12 +1,14 @@
 class_name InventoryItemSlot extends Control
 
 @export_group("Local")
+@export var name_label: Label
 @export var texture_rect: TextureRect
 
 var item: InventoryItem = null
 
 func set_item(item: InventoryItem) -> void:
 	texture_rect.texture = item.image
+	name_label.text = item.name
 	self.item = item
 
 func is_item(item: InventoryItem) -> bool:
