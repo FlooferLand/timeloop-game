03b05574637d56dbf79329686eebf8216f2ee3b7
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-08-03 16:07:11 +0300

    Fix audio bug with cutscenes
    Audio kept continuing

--- levels/outside/skippo.gd
+++ levels/outside/skippo.gd
@@ -7,6 +7,7 @@ const BuildingScene := preload("uid://douk4eknvrii7")
 const TIMES_TO_PRESS: int = 8
 
 @export var skip_feedback: AudioStreamPlayer
+@export var to_stop: Array[Node] = []
 
 var times_pressed: int = 0
 
@@ -17,4 +18,9 @@ func _input(event: InputEvent) -> void:
 		skip_feedback.pitch_scale += 0.1
 		skip_feedback.volume_db += 0.1
 		if times_pressed >= TIMES_TO_PRESS:
+			for stopping in to_stop:
+				if stopping == null:
+					continue
+				if stopping.has_method("stop"):
+					stopping.call("stop")
 			get_tree().change_scene_to_packed(BuildingScene)
