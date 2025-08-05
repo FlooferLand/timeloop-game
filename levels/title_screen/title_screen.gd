761536a77f66c2b7ad9cc31e0fc2f43ffa552272
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-08-05 13:52:32 +0300

    Clean up main menu and add animations

--- levels/title_screen/title_screen.gd
+++ levels/title_screen/title_screen.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+const IntroScene := preload("uid://cjgtv4eflexi6")
+
 @export var navcom_voice: AudioStreamPlayer
 @export var radio_noise: AudioStreamPlayer
+@export var anim_player: AnimationPlayer
+@export var respond_button: Button
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
@@ -11,9 +15,14 @@ func _ready() -> void:
 	get_tree().create_timer(2.0).timeout.connect(func() -> void:
 		radio_noise.play()
 	)
+	anim_player.animation_finished.connect(func(anim_name: String) -> void:
+		if anim_name == "respond":
+			get_tree().change_scene_to_packed(IntroScene)
+	)
 
 func _on_play_game_pressed() -> void:
-	get_tree().change_scene_to_file("uid://cjgtv4eflexi6")
+	anim_player.play("respond")
+	respond_button.disabled = true
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
