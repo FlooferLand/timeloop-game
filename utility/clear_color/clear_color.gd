ae17462b894446fa0bdb2eda217bd9e1c404eb80
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-08-05 10:25:29 +0300

    Improve clear_color utility

--- utility/clear_color/clear_color.gd
+++ utility/clear_color/clear_color.gd
@@ -1,4 +1,11 @@
 extends Node
 
+@export var color := Color.BLACK
+
+@export_group("Local")
+@export var canvas_layer: CanvasLayer
+@export var color_rect: ColorRect
+
 func _enter_tree() -> void:
-	($CanvasLayer as CanvasLayer).visible = true
+	color_rect.color = color
+	canvas_layer.visible = true
