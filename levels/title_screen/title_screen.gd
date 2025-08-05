8b0c1d06777cffd32e77653dcb268b19ae3d28ac
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-08-05 16:09:22 +0300

    Add audio fading to title screen

--- levels/title_screen/title_screen.gd
+++ levels/title_screen/title_screen.gd
@@ -3,12 +3,16 @@ extends Node2D
 const IntroScene := preload("uid://cjgtv4eflexi6")
 
 @export var navcom_voice: AudioStreamPlayer
+@export var music: AudioStreamPlayer
 @export var radio_noise: AudioStreamPlayer
 @export var anim_player: AnimationPlayer
 @export var respond_button: Button
 
+var music_playback: AudioStreamPlaybackInteractive
+
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	music_playback = music.get_stream_playback() as AudioStreamPlaybackInteractive
 	get_tree().create_timer(4.0).timeout.connect(func() -> void:
 		navcom_voice.play()
 	)
@@ -23,6 +27,7 @@ func _ready() -> void:
 func _on_play_game_pressed() -> void:
 	anim_player.play("respond")
 	respond_button.disabled = true
+	music_playback.switch_to_clip(2)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
