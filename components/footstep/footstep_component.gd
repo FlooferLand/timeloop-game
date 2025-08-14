61b6311deb0bc7089888868c858a20efbd979064
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-08-14 15:40:22 +0300

    Add footstep particles

--- components/footstep/footstep_component.gd
+++ components/footstep/footstep_component.gd
@@ -8,8 +8,10 @@ extends Node2D
 		if audio_player != null:
 			audio_player.stream = sounds
 
-@onready var audio_player: AudioStreamPlayer2D = $AudioPlayer
-@onready var timer: Timer = $Timer
+@export_group("Local")
+@export var audio_player: AudioStreamPlayer2D
+@export var timer: Timer
+@export var particles: GPUParticles2D
 
 var _playing := false
 var _initial_wait_time: float
@@ -19,6 +21,7 @@ func _ready() -> void:
 	audio_player.stream = sounds
 	timer.timeout.connect(func() -> void:
 		audio_player.play()
+		particles.restart()
 	)
 	if is_local:
 		audio_player.panning_strength = 0
@@ -35,5 +38,7 @@ func stop() -> void:
 func is_playing() -> bool:
 	return _playing
 
+# NOTE: This gets called constantly. Probably not good for performance-
 func set_speed(speed: float) -> void:
+	particles.modulate = (Color.WHITE.lerp(Color.TRANSPARENT, 0.85 - ((speed-1) * 0.6)))
 	timer.wait_time = _initial_wait_time / max(speed, 0.0)
